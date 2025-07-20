f5c941ceae9f6de9b58c5ec0070a09f572d4e477
Senam <senam@Senams-MacBook-Pro.local>
2025-07-20 13:58:17 +0100

    added static typing to database connection class

--- database/db_handler.py
+++ database/db_handler.py
@@ -1,26 +1,35 @@
+from typing import Optional
 import psycopg2
+from psycopg2.extensions import connection as PGConnection, cursor as PGCursor
 
 class DatabaseConnection:
     """
     Singleton class for managing database connections.
     """
 
-    _instance = None
+    _instance: Optional ["DatabaseConnection"] = None
+    _connection: Optional[PGConnection] = None
+    _cursor: Optional[PGCursor]
 
-    def __new__(cls, *args, **kwargs):
+
+    def __new__(cls):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
             cls._instance._connection = None #TO DO - initialize database connection here 
         return cls._instance
+
+    # The above can be changed to a connection pool when you have multiple users
     
 
     def __init__(self):
-        self.connection = self._instance.connection
-        self.cursor = self._instance.cursor
+        if not getattr(self, '_initialized', False):
+            self.connection = self._instance.connection
+            self.cursor = self._instance.cursor
+            self._initialized = True
 
 
-    def query(self, query, params=None):
-
+    def query(self, query: str, params: Optional[tuple] = None) -> Optional[list]:
+        return list
 
     def disconnect(self):
         """
